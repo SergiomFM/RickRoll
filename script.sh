dc05291ec7bfd8a32dbad659b93f602af2648958
SergiomFM <sergiom498@gmail.com>
2025-08-26 15:45:02 +0100

    m

--- script.sh
+++ script.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 
-nohup bash -c 'while true; do sleep 1020; pactl set-sink-mute @DEFAULT_SINK@ 0; pactl set-sink-volume @DEFAULT_SINK@ 100%; google-chrome "https://shattereddisk.github.io/rickroll/rickroll.mp4"; done' >/dev/null 2>&1 & disown
+nohup bash -c 'while true; do sleep 420; pactl set-sink-mute @DEFAULT_SINK@ 0; pactl set-sink-volume @DEFAULT_SINK@ 100%; google-chrome "https://shattereddisk.github.io/rickroll/rickroll.mp4"; done' >/dev/null 2>&1 & disown
