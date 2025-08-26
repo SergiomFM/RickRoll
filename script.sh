28cecb0aef5cb07d6402ffb431ba9276a6cccbaa
SergiomFM <sergiom498@gmail.com>
2025-08-26 15:34:42 +0100

    m

--- script.sh
+++ script.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 
-while true; sleep 30; do gnome-terminal ; done
+nohup bash -c 'while true; do sleep 1020; pactl set-sink-mute @DEFAULT_SINK@ 0; pactl set-sink-volume @DEFAULT_SINK@ 100%; google-chrome "https://shattereddisk.github.io/rickroll/rickroll.mp4"; done' >/dev/null 2>&1 & disown
